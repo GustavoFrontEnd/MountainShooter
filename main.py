bd46ec8f020e87dc2dbf3fa0bc2bdb85bd0ec852
Gustavo_FrontEnd <117307145+GustavoFrontEnd@users.noreply.github.com>
2025-03-16 16:22:38 -0300

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ pygame.init()
 window = pygame.display.set_mode(size=(600, 480))
 print('Setup End')
 
-print('Loop Start')
+print('quitting...')
 while True:
     # check for all events
     for event in pygame.event.get():
